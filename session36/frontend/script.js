bad2fc2a346532d1f0783a0f3b547e0710121d4d
webmanster <arsalan_habib@yahoo.com>
2016-10-30 16:04:38 -0400

    Update script.js
    Add new request for landmarks for initial load of pages.

--- session36/frontend/script.js
+++ session36/frontend/script.js
@@ -1,26 +1,22 @@
 /* global $ */
 /* global google */
 
-$(document).ready(function() {
+var lng, lat;
 
-    var lng, lat;
+// Mississauga Central Library
+lng = -79.6436414, lat = 43.5875534;
 
-    // Mississauga Central Library
-    lng = -79.6436414;
-    lat = 43.5875534;
-
-    var pos = new google.maps.LatLng(lat, lng);
-
-    var mapProperties = {
+var pos = new google.maps.LatLng(lat, lng);
+var mapProperties = {
         "center": pos,
-        "zoom": 17,
+        "zoom": 16,
         "mapTypeId": google.maps.MapTypeId.ROADMAP
     };
+	
+var div = document.getElementById('divMap');
+var map = new google.maps.Map(div, mapProperties);
 
-    var div = document.getElementById('divMap');
-    var map = new google.maps.Map(div, mapProperties);
-
-    var marker = new google.maps.Marker({
+var marker = new google.maps.Marker({
         "position": pos,
         "icon": {
             "path": google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
@@ -30,8 +26,39 @@ $(document).ready(function() {
         },
         "map": map
     });
+	
+$(document).ready(function() {
+	// query for landmark types and generate the drop down options
+	$.get('http://127.0.0.1:3000/getProperty', function(resp){
+		$.each(resp.typedesc, function(key, value) {              
+			$('#ddPropertyTypes').append($('<option>').val(value._id).text(value._id));     
+		});
+	});
+	
+	// code from the first session on geoJSON
+    /*$.get('http://127.0.0.1:3000', function(resp){
+       map.data.addGeoJson(resp);
+       map.data.setStyle(function(feature) {
+           return {
+               "title": feature.getProperty('LANDMARKNA'),
+               "icon": {
+                   "path": google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+                   "scale": 5,
+                   "strokeWeight":2,
+                   "strokeColor":"#0000FF"
+                },
+           };
+       });
+    });*/
+});
 
-    $.get('http://use-own-server-url-here/', function(resp){
+// event handler for select element onchange event
+function filterByPropertyType(propertyType){
+	map.data.forEach(function(feature){
+		map.data.remove(feature);
+	});
+	$.get('http://127.0.0.1:3000/filterByPropertyType=' + propertyType, function(resp){
+		console.log(resp);
        map.data.addGeoJson(resp);
        map.data.setStyle(function(feature) {
            return {
@@ -45,6 +72,4 @@ $(document).ready(function() {
            };
        });
     });
-
-
-});
+}
