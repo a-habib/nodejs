868a2eeec760c21a1bda5074479d29d1317f2e0f
webmanster <arsalan_habib@yahoo.com>
2016-10-30 15:33:04 -0400

    Update server.js

--- session36/server.js
+++ session36/server.js
@@ -16,7 +16,68 @@ server.use(function(req, res, next) {
     next();
 });
 
-server.get('/', function(req,res){
+// route to query differnt types of landmarks available
+server.get('/getProperty', function(req,res){
+	console.log("Requesting " + req.url);
+	mongodb.connect(connStr, function(err, dbConn) {
+		if (err) throw err;
+		var coll = dbConn.collection('lm');
+		var groupStage = {"$group" : {"_id" : "$properties.TYPEDESC"} };
+		var sortStage = {"$sort" : {"_id" : 1}};
+		//var limitStage = {"$limit" : 5};
+		var cursor = coll.aggregate([groupStage, sortStage]);
+		cursor.toArray( function(err,docs){
+            if (err) throw err;
+            res.json({"typedesc":docs});
+            dbConn.close();
+        });
+	});
+});
+
+// route to query all landmarks given a specific landmark type from the request
+server.get('/filterByPropertyType=*', function(req,res){
+	console.log("Requesting " + req.url);
+	var parameter = decodeURIComponent(req.url.slice(req.url.indexOf('=')+1));
+	console.log(parameter);
+    mongodb.connect(connStr, function(err, dbConn) {
+        if (err) throw err;
+        var coll = dbConn.collection('lm');
+        
+        // this filter has two conditions on fields 
+        // "properties.TYPEDESC" and "geometry"
+        // implicit 'and' (as opposed to 'or')
+        var query = { 
+            "properties.TYPEDESC": parameter,
+            "geometry" : { 
+                "$nearSphere" : { 
+                    "$geometry" : { 
+                        "type" : "Point" ,
+                        "coordinates" : [ -79.6436414, 43.5875534 ] 
+                    } ,
+                    "$maxDistance" : 1500
+                } 
+            } 
+        };
+        
+        // note re:regular expression operator
+        // above can use something like: "properties.TYPEDESC" : { "$regex": /^police/i } ,
+        // matches any TYPEDESC that starts with the letters 'police', case insensitive
+        
+        var proj = {"_id":0};
+        
+        var cursor = coll.find(query, proj);
+        
+        cursor.toArray( function(err,docs){
+            if (err) throw err;
+            res.json({"type":"FeatureCollection","features":docs});
+            dbConn.close();
+        });
+       
+    });
+});
+
+/*server.get('/', function(req,res){
+	console.log("Requesting " + req.url);
     mongodb.connect(connStr, function(err, dbConn) {
         if (err) throw err;
         var coll = dbConn.collection('lm');
@@ -52,4 +113,4 @@ server.get('/', function(req,res){
         });
        
     });
-});
+});*/
